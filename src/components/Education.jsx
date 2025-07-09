04a74776536ce67a0646024e62574f467f295d62
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-07-09 12:26:50 -0700

    update on certification module

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import oau from "../images/oauLogo.png";
 import { education } from "../data/db";
+import Certificates from "./Certificates";
 
 const Education = () => {
   return (
     <main className="bg-gradient-to-b from-[#0f2f56] via-[#0b1a0a] to-[#010610] pt-10">
+      <Certificates />
       <div className="py-[3rem] flex flex-col text-white max-w-[1240px] mx-auto px-4 md:px-12 items-center ">
         <h1 className="relative inline-block text-xl md:text-3xl lg:text-3xl pb-3 font-bold">
           EDUCATION
